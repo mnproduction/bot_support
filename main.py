61d67096aea7929b1465227c9495619977c3e9b2
Nick <qdqd@acvvrv.com>
2024-08-29 17:47:06 +0300

    modified:   main.py 	modified:   requirements.txt 	new file:   test_data/mykolanovosolov.pdf
    Add test data
    supressed deprecation warning "cleanup_tokenization_spaces"

--- main.py
+++ main.py
@@ -1,111 +1,53 @@
-from os import getenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
 from langchain_community.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.llms.ollama import Ollama
-from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
+import warnings
+warnings.filterwarnings("ignore", message=".*clean_up_tokenization_spaces.*")
 
 def init_knowledge_base(documents: list) -> FAISS:
-    """Initializes a knowledge base from the provided documents.
-
-    Args:
-        documents: A list of documents representing chunks of text from FAQ.pdf.
-
-    Returns:
-        A FAISS object containing the vectorized knowledge base and index for search.
-    """
-
-    # Create embeddings using the OpenAI model
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-
-    # Create a vectorized knowledge base using FAISS
+    """Initializes a knowledge base from the provided documents."""
+    embeddings = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": False},
+        # tokenizer_kwargs={"cleanup tokenization spaces": True}
+    )
     knowledge_base = FAISS.from_documents(documents, embeddings)
-
     return knowledge_base
 
 def get_answer_from_knowledge_base(query: str, knowledge_base: FAISS) -> str:
-    """Retrieves an answer to a query from the knowledge base.
-
-    Args:
-        query: The user's question.
-        knowledge_base: The FAISS object containing the knowledge base.
-
-    Returns:
-        The answer to the question found in the knowledge base.
-    """
-
+    """Retrieves an answer to a query from the knowledge base."""
     llm = Ollama(model="llama3.1")
-    
-    # Load the question-answering chain using the OpenAI large language model
-    chain = load_qa_chain(llm, chain_type="stuff")
-
-    # Perform a similarity search in the knowledge base and generate an answer
-    answer = chain.run(input_documents=knowledge_base.similarity_search(query), question=query)
-
-    return answer
+    retriever = knowledge_base.as_retriever()
+    qa_chain: RetrievalQA = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+    answer = qa_chain.invoke({"query": query})
+    return answer["result"]
 
 def load_local_pdf(file_path: str) -> list:
-    """
-    Load data from web PDF
-
-    Parameters
-    ----------
-    url : str
-        URL of the PDF file in Google Drive
-
-    Returns
-    -------
-    documents : list
-        A list of documents, each of which is the content of the PDF file
-
-    """
-    
+    """Load data from local PDF"""
     loader = PyPDFLoader(file_path)
     documents = loader.load()
-
     return documents
-
 
 def split_documents(documents: list) -> list:
-    """
-    Split documents into chunks for convenient processing
-
-    Parameters
-    ----------
-    documents : list
-        A list of documents
-
-    Returns
-    -------
-    documents : list
-        A list of documents, each of which is a chunk of the original document
-
-    """
-    
-    # Split the text into chunks for convenient processing
+    """Split documents into chunks for convenient processing"""
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,  # Chunk size (choose an optimal value)
-        chunk_overlap=200  # Overlap between chunks (helps to preserve the context)
+        chunk_size=1000,
+        chunk_overlap=200
     )
     documents = text_splitter.split_documents(documents)
-
     return documents
 
-
 def main():
-    doc = load_local_pdf("data/Internal_FAQ.pdf")
-    # print(doc)
+    doc = load_local_pdf("test_data/mykolanovosolov.pdf")
     splited = split_documents(doc)
-    # print(splited)
-    
     custom_knowledge_base = init_knowledge_base(splited)
-    # print(custom_knowledge_base)
-    
-    answer = get_answer_from_knowledge_base("What is CPC?", custom_knowledge_base)
+    answer = get_answer_from_knowledge_base("Who is Mykola?", custom_knowledge_base)
     print(answer)
-    pass
 
 if __name__ == "__main__":
     main()
